424ebff3c1a339ec20350dcb15a79bdcec9940fe
Meruem117 <1572677319@qq.com>
2021-08-13 10:22:24 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import $ from 'jquery'
+// import $ from 'jquery'
 
-const title = document.querySelector('#title')
-title.addEventListener('click', () => {
-    $(body).css('backgroundColor', 'red')
-})
+// const title = document.querySelector('#title')
+// title.addEventListener('click', () => {
+//     $(body).css('backgroundColor', 'red')
+// })
+
+import data from './index.json'
+import './index.css'
+import './index.less'
+
+console.log(data)
+
+console.log(123)
